719a906d754a4a5d6ac1e9625ff72cedcc20640b
Ryan Blackman <rblackman24@gmail.com>
2022-01-01 22:48:15 -0600

    wiring up next data hooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,38 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
+import { getDataHooksProps } from 'next-data-hooks';
+import useSiteConfig from '../hooks/data-hooks/useSiteConfig';
+
+interface Props {}
+
 export default function Home() {
-	return <p>Hello World</p>;
+	const { title } = useSiteConfig();
+
+	return (
+		<>
+			<h1>{title}</h1>
+			<p>Hello World</p>
+		</>
+	);
 }
+
+Home.dataHooks = [useSiteConfig];
+
+type StaticProps = Props & {
+	nextDataHooks: {
+		[k: string]: any;
+	};
+};
+
+export const getStaticProps: GetStaticProps<StaticProps> = async (context: GetStaticPropsContext) => {
+	const dataHooksProps = await getDataHooksProps({
+		context,
+		dataHooks: Home.dataHooks
+	});
+
+	return {
+		props: {
+			...dataHooksProps
+		},
+		revalidate: 60 * 60 * 12 // 12 hours
+	};
+};
