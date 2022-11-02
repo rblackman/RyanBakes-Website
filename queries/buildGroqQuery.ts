22c5a8e429ae580864b16f3a477deb6864f3fe79
Ryan Blackman <rblackman24@gmail.com>
2022-11-01 22:27:28 -0500

    change encode uri to encode uri component

--- queries/buildGroqQuery.ts
+++ queries/buildGroqQuery.ts
@@ -4,7 +4,7 @@ const dataset = process.env.SANITY_DATASET;
 const baseAddress = `https://${siteKey}.api.sanity.io/v1/data/query/${dataset}`;
 
 export default function buildGroqQuery(groq: string) {
-	const encodedQuery = encodeURI(groq).replaceAll('=', '%3D').replaceAll('&', '%26');
+	const encodedQuery = encodeURIComponent(groq);
 	const url = `${baseAddress}?query=${encodedQuery}`;
 	return url;
 }
