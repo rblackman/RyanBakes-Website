6f32ee2c078b518eb21d8dfbbccf4d91052f3cff
Ryan Blackman <rblackman24@gmail.com>
2022-11-30 18:24:19 -0600

    add more metadata

--- app/recipe/[slug]/head.tsx
+++ app/recipe/[slug]/head.tsx
@@ -4,6 +4,8 @@ import getSiteConfig from 'queries/getSiteConfig';
 import { use, useMemo } from 'react';
 import 'server-only';
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
 export default function Head({ params: { slug } }: { params: { slug: string } }) {
 	const { title: siteTitle } = use(getSiteConfig());
 	const {
@@ -12,31 +14,54 @@ export default function Head({ params: { slug } }: { params: { slug: string } })
 		tags,
 		_createdAt: created,
 		_updatedAt: updated,
-		openGraphImage: { asset }
+		openGraphImage: { asset, alt }
 	} = use(getRecipeBySlug(slug));
+
+	const url = useMemo(() => `${baseUrl}/recipe/${slug}`, [slug]);
 	const keywords = useMemo(() => tags.join(','), [tags]);
 
 	const combinedTitle = `${siteTitle} | ${recipeTitle}`;
-	const ogImage = useImageBuilder(asset).buildUrlWithOptions({
+	const builder = useImageBuilder(asset);
+	const ogImage = builder.buildUrlWithOptions({
 		width: 1200,
 		height: 627,
 		quality: 0.6
 	});
+
+	const twitterImage = builder.buildUrlWithOptions({
+		width: 4096,
+		height: 2048,
+		quality: 0.6
+	});
+
 	return (
 		<>
 			<title>{combinedTitle}</title>
 			<meta name="description" content={description} />
 			<meta name="keywords" content={keywords} />
-			<meta name="og:title" content={recipeTitle} />
+
+			<meta name="og:url" content={url} />
 			<meta name="og:type" content="article" />
+			<meta name="og:title" content={recipeTitle} />
+			<meta name="og:description" content={description} />
+			<meta property="og:image" content={ogImage} />
+			<meta property="og:image:alt" content={alt} />
+			<meta property="og:image:width" content="1200" />
+			<meta property="og:image:height" content="627" />
 			<meta name="article:published_time" content={created} />
 			<meta name="article:modified_time" content={updated} />
+
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta property="twitter:domain" content={baseUrl} />
+			<meta property="twitter:url" content={url} />
+			<meta name="twitter:title" content={recipeTitle} />
+			<meta name="twitter:description" content={description} />
+			<meta name="twitter:image" content={twitterImage} />
+			<meta name="twitter:image:alt" content={alt} />
+
 			{tags.map((tag) => (
 				<meta name="article:tag" content={tag} key={tag} />
 			))}
-			<meta property="og:image" content={ogImage} />
-			<meta property="og:image:width" content="1200" />
-			<meta property="og:image:height" content="627" />
 		</>
 	);
 }
